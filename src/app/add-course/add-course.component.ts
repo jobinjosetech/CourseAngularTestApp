35ebed0154b93d99aadc0ad29543b929eb595284
jobin <jobinjose.in@gmail.com>
2022-12-07 05:17:07 +0530

    databases

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-add-course',
@@ -6,13 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-course.component.css']
 })
 export class AddCourseComponent {
-  courseTitle = ""
-  courseDescription = ""
-  courseDuration = ""
-  courseDate = ""
-  courseVenue = ""
+  constructor(private api: ApiService,) { }
+  title = ""
+  description = ""
+  duration = ""
+  date = ""
+  venue = ""
 
-  readValues = ()=>{
-    
+  readValues = () => {
+    let courseData = { "title": this.title, "description": this.description, "duration": this.duration, "date": this.date, "venue": this.venue }
+    this.api.addCourse(courseData).subscribe(
+      (response: any) => {
+        console.log(response)
+        if (response.status == "success") {
+          alert('Course added successfully')
+          this.title = ""
+          this.description = ""
+          this.duration = ""
+          this.date = ""
+          this.venue = ""
+        } else {
+          alert('Error in adding course')
+        }
+      }
+    )
+    console.log(courseData)
   }
+
 }
+
